b33e1db0e3110cd781efd4f2bb6cb946d14b6984
Daniel Bugl <me@omnidan.net>
2016-07-11 18:35:28 -0400

    use .chat instead of .channel

--- src/commands.js
+++ src/commands.js
@@ -3,12 +3,12 @@ export default function makeCommands (client) {
     // TODO: standardize these commands and document in coffea
     //       e.g. message(...), me(), photo(...), ...
     'message': (event) =>
-      client.say(event.channel, event.text),
+      client.say(event.chat, event.text),
     'send': (event) =>
       client.send(...event.args),
     'join': (event) =>
-      client.join(event.channel + (event.password ? ' ' + event.password : '')),
+      client.join(event.chat + (event.password ? ' ' + event.password : '')),
     'part': (event) =>
-      client.part(event.channel)
+      client.part(event.chat)
   }
 }
